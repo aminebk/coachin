4594d8eca6985929e41e655e5f544c22f352fdf0
Amine <amine.boukdeir@gmail.com>
2014-05-04 18:05:33 +0100

    HTML

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+    "use strict";	
+	
+	
+	$('#home-slider').flexslider({
+        animation: "slide",
+        directionNav: false,
+        controlNav: true,
+        pauseOnHover: true,
+        slideshowSpeed: 4000,
+        direction: "horizontal" //Direction of slides
+    });
+	
+});
